09f3826cb8b50b51ee952d8be169d44ea3beed5d
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-08 10:03:26 +0530

    RoboShop Project

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z03100071FR6KSIBKYEU8"
 DOMAIN_NAME="daws84s.online"
 
-for instance in ${INSTANCES[@]}
+for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-09e1c153181924f57 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != frontend ]
