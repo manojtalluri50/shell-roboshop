65bd012bba560daf414ea1bf29dc03ab6b88f8ce
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-08 18:09:00 +0530

    RoboShop Project

--- catalogue.sh
+++ catalogue.sh
@@ -78,6 +78,7 @@ VALIDATE $? "Copying MongoDB Repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
+# if the status is less than 1 means data is not loaded and we need to load the data and more than 1 means data is already loaded into the mongodb
 STATUS=$(mongosh --host mongodb.daws84s.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
