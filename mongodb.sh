4b5df71af9bf9c1db35be187d3b1e8c7a5d9eb1f
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-08 12:05:20 +0530

    RoboShop Project

--- mongodb.sh
+++ mongodb.sh
@@ -34,17 +34,17 @@ VALIDATE(){
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copying MongoDB Repo"
 
-dnf install mongodb-org -y &>>LOG_FILE
+dnf install mongodb-org -y &>>$LOG_FILE
 VALIDATE $? "Installing mongodb server"
 
-systemctl enable mongod &>>LOG_FILE
+systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "Enabling MongoDB"
 
-systemctl start mongod &>>LOG_FILE
+systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "Starting MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Editing MongoDB conf file for remote connections"
 
-systemctl restart mongod &>>LOG_FILE
+systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "Restarting MongoDB"
