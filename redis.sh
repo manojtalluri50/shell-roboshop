1baae5dc6695c0b21651a4c5c64822c0c2c90d40
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-08 18:45:25 +0530

    RoboShop Project

--- redis.sh
+++ redis.sh
@@ -48,5 +48,10 @@ VALIDATE $? "Edited redis.conf to accept remote connections"
 systemctl enable redis &>>$LOG_FILE
 VALIDATE $? "Enabling Redis"
 
-systemctl start redis 
+systemctl start redis &>>$LOG_FILE
 VALIDATE $? "Starting Redis"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script execution completed successfully, &Y time taken: $TOTAL_TIME in seconds $N" | tee -a $LOG_FILE
