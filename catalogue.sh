37f0e0981f521a69bf1f160db14f74811038e744
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-08 17:45:03 +0530

    RoboShop Project

--- catalogue.sh
+++ catalogue.sh
@@ -41,7 +41,7 @@ VALIDATE $? "Enabling nodejs:20"
 dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing nodejs:20"
 
-id roboshop
+id roboshop &>>$LOG_FILE
 if [ $? -ne  0 ]
 then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
